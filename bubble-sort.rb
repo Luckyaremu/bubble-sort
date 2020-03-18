6938cefca9c579d93c3fc538674b1f5d0c8f1b65
kotoku-code <kennedy.kotoku@gmail.com>
2020-03-18 14:11:59 +0100

    corrected rubocop errors

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,24 +1,21 @@
 def bubble_sort(arr)
+  sorted = true
 
-    sorted = true
-    
-        while sorted
-    
-         sorted = false
-    
-         (arr.length-1).times do |i|
-    
-            if arr[i] > arr[i+1]
-    
-              arr[i], arr[i+1] = arr[i+1], arr[i]
-    
-              sorted = true
-            end
-           end
-         end
-        arr
-        end
-    
-        a = [10, 1, 2, 5, 3, 6, 4, 9, 8, 10, 4]
-    
-    bubble_sort(a)
+  while sorted
+
+    sorted = false
+
+    (arr.length - 1).times do |i|
+      next unless arr[i] > arr[i + 1]
+
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+
+      sorted = true
+    end
+  end
+  arr
+end
+
+a = [10, 1, 2, 5, 3, 6, 4, 9, 8, 10, 4]
+
+bubble_sort(a)
